8c6463d73bb0e0eb5233f64e38c706a44ce55b4d
dhiren-rajesh <dhirenrajeshk@gmail.com>
2026-01-01 16:42:28 +0530

    Defining the API Key

--- src/ai_agent.py
+++ src/ai_agent.py
@@ -1,11 +1,18 @@
 import os
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
+from google.generativeai import genai
 
 load_dotenv()
 
-def generate_project_plan(project_details: str) -> str:
-    # Placeholder function to simulate AI-generated project plan
-    prompt = ChatPromptTemplate("""Generate a detailed project plan for the following project: {project_details}. 
+API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=API_KEY)
+
+prompt_temmplate = ChatPromptTemplate("""Generate a detailed project plan for the following project: {project_details}. 
                                 Return a small description of the plan followed by the split in a tabular format. 
                                 The split should include the tasks side by side with the estimated time for each task.""")
+
+def generate_project_plan(project_details: str) -> str:
+    # Placeholder function to simulate AI-generated project plan
+    prompt = prompt_temmplate.format(project_details=project_details)
+    
