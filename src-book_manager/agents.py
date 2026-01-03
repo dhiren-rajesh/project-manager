2d379039281f415c400b71ebf9a8674c26831763
dhiren-rajesh <dhirenrajeshk@gmail.com>
2026-01-03 17:11:34 +0530

    Added method to return stock

--- src-book_manager/agents.py
+++ src-book_manager/agents.py
@@ -1,18 +1,39 @@
 from google import genai
+from google.genai import types
+from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 client = genai.Client()
 
-response_stock = client.models.generate_content(
-    model="gemini-2.5-flash-lite",
-    contents="Maintain the stock of books in a library system and return it. Also mention Available or not based on stock."
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR / "db.json"
+db_text = DB_PATH.read_text(encoding="utf-8")
+
+
+prompt_stock = (
+    "1. Maintain the stock of books in a library system and return it. "
+    "2. Mention whether the book is available or not, based on stock."
+    "3. Do it in a tabular format."
+    "4. Do not create books which are not in the DB."
 )
 
-response_input = client.models.generate_content(
+prompt_search = "Return all the books which include the users input."
+
+response_stock = client.models.generate_content(
     model="gemini-2.5-flash-lite",
-    contents="Return the books by the user input."
+    contents=[
+        f"Library database:\n{db_text}",
+        prompt_stock
+    ]
+)
+
+response_search = client.models.generate_content(
+    model="gemini-2.5-flash-lite",
+    contents=[
+        f"Library database:\n{db_text}",
+        prompt_search
+    ]
 )
