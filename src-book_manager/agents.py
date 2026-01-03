24b29118d1c4480a03b4a6f1d0cb28db0a6aad7d
dhiren-rajesh <dhirenrajeshk@gmail.com>
2026-01-03 18:05:51 +0530

    Added method to return search and refined search

--- src-book_manager/agents.py
+++ src-book_manager/agents.py
@@ -16,11 +16,25 @@ db_text = DB_PATH.read_text(encoding="utf-8")
 prompt_stock = (
     "1. Maintain the stock of books in a library system and return it. "
     "2. Mention whether the book is available or not, based on stock."
-    "3. Do it in a tabular format."
-    "4. Do not create books which are not in the DB."
+    "3. Output must be in tabular format."
+    "4. Do NOT create books which are not in the DB."
+    "5. ONLY return the name, stock and available columns."
 )
+user_input = "id=4, To Kill a Mockingbird"
 
-prompt_search = "Return all the books which include the users input."
+prompt_search = f"""
+    ### User Input
+    {user_input}
+
+    ### Rules
+    1. ONLY return books if the user input contains "id=<number>" exactly.
+    2. If the rule is violated, return: "No book found".
+    3. Match ONLY the given ID.
+    4. Output must be in tabular format.
+    5. Include all columns EXCEPT stock.
+    6. Do NOT create books which are not in the DB.
+    7. If user input is comma separted, return the EVERY book which satisfies ANY user inputs.
+"""
 
 response_stock = client.models.generate_content(
     model="gemini-2.5-flash-lite",
@@ -37,3 +51,7 @@ response_search = client.models.generate_content(
         prompt_search
     ]
 )
+
+print("Stock: \n", response_stock.text)
+print("Search:\n ", response_search.text)
+
